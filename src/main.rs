bf80e4e66553ce8d1fe2ec491d1acd45dc7a9e94
Zakki0925224 <zakki0925224@gmail.com>
2025-03-12 17:02:45 +0900

    [Add]: Added REPL mode

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,50 @@ mod vm;
 
 #[derive(Parser, Debug)]
 struct Args {
-    path: String,
+    path: Option<String>,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let file = File::open(args.path)?;
+    match args.path {
+        Some(path) => run_file(&path),
+        None => run_repl(),
+    }
+}
+
+fn run_file(path: &str) -> anyhow::Result<()> {
+    let file = File::open(path)?;
     let proto = parse::ParseProto::load(file)?;
     vm::ExeState::new().execute(&proto)?;
 
     Ok(())
+}
+
+fn run_repl() -> anyhow::Result<()> {
+    println!("Lua interpreter (Exit with Ctrl+C)");
+
+    let mut exe_state = vm::ExeState::new();
+    let mut editor = rustyline::DefaultEditor::new()?;
+    loop {
+        let line = editor.readline("\n>> ")?;
+
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        editor.add_history_entry(line.as_str())?;
+
+        // parse
+        let proto = match parse::ParseProto::load(line.as_bytes()) {
+            Ok(proto) => proto,
+            Err(e) => {
+                eprintln!("Parse error: {}", e);
+                continue;
+            }
+        };
+        // exectute
+        if let Err(e) = exe_state.execute(&proto) {
+            eprintln!("Execution error: {}", e);
+        }
+    }
 }
